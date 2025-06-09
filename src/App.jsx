edd1c03b51441fd9693348c5eabe1b21bcb7d2ff
Ajal RP <ajal.rp@ngxptechnologies.com>
2025-06-09 19:58:46 +0530

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Container } from "@chakra-ui/react";
+import "./App.css";
+
+function App() {
+  return (
+    <Container>
+      <div></div>
+    </Container>
+  );
+}
+
+export default App;
