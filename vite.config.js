9fbda405b57d7ba5e17dbe6e5d0250e2bb2ffee9
Ajal RP <ajal.rp@ngxptechnologies.com>
2025-06-12 18:41:09 +0530

    updated deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "react-online-shopping/",
+  base: "/react-online-shopping/",
   plugins: [react(), tsconfigPaths()],
 });
