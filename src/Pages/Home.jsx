b91a37dcc4f00509d213ef01f62c4cce3600375e
Ajal RP <ajal.rp@ngxptechnologies.com>
2025-06-12 18:22:25 +0530

    updated details page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
     fetch("/data.json")
       .then((response) => response.json())
       .then((data) => {
-        console.log("Data :=> ", data);
+        console.log("Products :=> ", data);
         setProducts(data);
       });
   }, []);
